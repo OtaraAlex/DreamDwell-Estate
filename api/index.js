6cd095457cb4e2541da23488eef17e5a5d61e6f0
OtaraAlex <mbechealex61@gmail.com>
2023-11-29 19:27:11 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
